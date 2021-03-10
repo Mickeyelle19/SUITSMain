44cae8dcf043b4b3dde9eca7ae31575bba7b4518
Data-Log0 <80176457+Data-Log0@users.noreply.github.com>
2021-03-10 14:04:50 -0600

    Change files into C# files

---
+++ LightingSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SACSUITSMain
+{
+    public class LightingSettings
+    {
+        public LightingSettings()
+        {
+            //Class constructor, leave empty
+        }
+
+        //Rest of the code for this class goes 
+        //below, all methods, variables, 
+        //functions, etc.
+    }
+}
---
+++ ScienceSamplingProgram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SACSUITSMain
+{
+    class ScienceSamplingProgram
+    {
+        public ScienceSamplingProgram()
+        {
+            //Class constructor, leave empty
+        }
+
+        //Rest of the code for this class goes 
+        //below, all methods, variables, 
+        //functions, etc.
+    }
+}
