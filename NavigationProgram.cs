74ee547b16824abfd11981ec0c43d0d3f608feb3
Data-Log0 <jpham14@student.alamo.edu>
2021-03-15 14:44:40 -0500

    Added Time Elapsed Timer

--- NavigationProgram.cs
+++ NavigationProgram.cs
@@ -13,6 +13,25 @@ namespace SACSUITSMain
             //Test
             //Class constructor, leave empty
         }
+        static void stopWatch()
+        {
+            bool start = true;
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            while (start == true)
+            {
+                Thread.Sleep(1000);
+                TimeSpan ts = watch.Elapsed;
+                string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                 ts.Hours, ts.Minutes, ts.Seconds,
+                 ts.Milliseconds / 10);
+                Console.WriteLine("Time elapsed: " + elapsedTime);
+            }
+            Console.ReadLine();
+            watch.Stop();
+            start = false;
+        }
 
         //Rest of the code for this class goes 
         //below, all methods, variables, 
